4b1e5e64b68e48f8910f14c03047d7125b03c000
Dallin Parker <dallin.r.parker@gmail.com>
2017-06-08 12:21:08 -0600

    finished project

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
-const auth0 = require('passport-auth0');
+const Auth0Strategy = require('passport-auth0');
 const bodyParser = require('body-parser');
 const massive = require('massive');
-const massifier = require('dm-massifier')('postgresql://localhost:5432/user_test');
+const massifier = require('dm-massifier')(process.env.DB_CONN);
 const cors = require('cors');
 const port = 3000;
 
@@ -18,11 +19,71 @@ app.use(cors());
 app.use(session({
   resave: true,
   saveUninitialized: true,
-  secret: 'keyboardcat'
+  secret: process.env.SESSION_SECRET
 }))
 app.use(passport.initialize());
 app.use(passport.session());
 
+
+
+passport.use(new Auth0Strategy({
+   domain:       process.env.DOMAIN,
+   clientID:     process.env.CLIENT_ID,
+   clientSecret: process.env.CLIENT_SECRET,
+   callbackURL:  '/auth/callback'
+  },
+  (accessToken, refreshToken, extraParams, profile, done) => {
+    //Find user in database
+    req.db.getUserByAuthId([profile.id], (err, user) => {
+      user = user[0];
+      if (!user) { //if there isn't one, we'll create one!
+        console.log('CREATING USER');
+        req.db.createUserByAuth([profile.displayName, profile.id], (err, user) => {
+          console.log('USER CREATED', userA);
+          return done(err, user[0]); // GOES TO SERIALIZE USER
+        })  //make sure the you pass ERR or Null first or else it will assume it's an err when it's the user object.
+      } else { //when we find the user, return it
+        console.log('FOUND USER', user);
+        return done(err, user);
+      }
+    })
+  }
+));
+
+passport.serializeUser(function(userA, done) {
+  console.log('serializing', userA);
+  var userB = userA;
+  //Things you might do here :
+   //Serialize just the id, get other information to add to session,
+  done(null, userB); //PUTS 'USER' ON THE SESSION
+});
+
+//USER COMES FROM SESSION - THIS IS INVOKED FOR EVERY ENDPOINT
+passport.deserializeUser(function(userB, done) {
+  var userC = userC;
+  //Things you might do here :
+    // Query the database with the user id, get other information to put on req.user
+  done(null, userC); //PUTS 'USER' ON REQ.USER
+});
+
+
+
+app.get('/auth', passport.authenticate('auth0'));
+
+app.get('/auth/callback',
+  passport.authenticate('auth0', {successRedirect: '/'}), function(req, res) {
+    res.status(200).send(req.user);
+})
+
+app.get('/auth/me', function(req, res) {
+  if (!req.user) return res.sendStatus(404);
+  res.status(200).send(req.user);
+})
+
+app.get('/auth/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+})
 
 
 
